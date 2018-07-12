8c370cdaa9e647de112ec1c4cb627b52a30518d4
Petr Holasek <holasekp@gmail.com>
2018-07-13 00:43:28 +0200

    lockscreen: rest of screens added
    * operator can use space for moving around in loop

--- js/lockscreen.js
+++ js/lockscreen.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //TODO put PIN and time to conf file
     var secret = 1234;
     var bar1 = new ldBar("#loadbar");
     bar1.set(0);
@@ -6,19 +7,56 @@ $(document).ready(function() {
     var start = 3 * 60 * 1000;
     var curr = start;
 
-    $("#startdia").dialog();
+    var slideIndex = 0;
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function showSlides(n) {
+        var i;
+        var slides = $('.subscreen').toArray();
+        console.log(slides.length);
 
-    setInterval(function() {
-        bar1.set(100 - Math.round(curr / start * 100));
-        curr -= 1000;
-        },
-     1000);
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        for (i = 0; i < slides.length; i++) {
+            $(slides[i]).hide();
+        }
+
+        // Start loading
+        if (slideIndex == 3) {
+            setInterval(function() {
+                bar1.set(100 - Math.round(curr / start * 100));
+                curr -= 1000;
+                },
+             1000);
+        }
+
+        $(slides[slideIndex-1]).show();
+        //$(slides[slideIndex-1]).css("display", "flex");
+    }
+
+
+
+    (function blink() {
+      $('#startScreen').fadeOut(1000).fadeIn(1000, blink);
+    })();
+
+
 
     $("#pin").on('input', function() {
-        $("#status").text("");
         var code = $("#pin").val();
         if (code.length == 4 && code == secret) {
-                $("#status").text("UNLOCKED");
+                //$("#status").text("UNLOCKED");
+                plusSlides(1);
         }
     });
+
+
+    $(document).keypress(function( event ) {
+        if ( event.which == 32 ) {
+            plusSlides(1);
+        }
+    });
+
 });
